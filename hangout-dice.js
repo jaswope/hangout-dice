2ca205f4afc7c5af18773ac87c85bf8335f9e2d8
Taylor <taylorshuler@aol.com>
2013-05-05 15:27:29 -0400

    Refined code

--- hangout-dice.js
+++ hangout-dice.js
@@ -1,48 +1,53 @@
-var HangoutDice = (function() {
-  var id = null;
+var HangoutDice;
 
-  var dice = {};
-  var callbacks = {};
-
-  function Mash() {
-    var n = 0xefc8249d;
- 
-    var mash = function(data) {
+HangoutDice = (function() {
+  var Alea, Mash, callbacks, dice, id, rollKey;
+  
+  Mash = function() {
+    var mash, n;
+    n = 0xefc8249d;
+    
+    mash = function(data) {
+      var h, i;
       data = data.toString();
-      for (var i = 0; i < data.length; i++) {
+      i = 0;
+      while (i < data.length) {
         n += data.charCodeAt(i);
-        var h = 0.02519603282416938 * n;
+        h = 0.02519603282416938 * n;
         n = h >>> 0;
         h -= n;
         h *= n;
         n = h >>> 0;
         h -= n;
-        n += h * 0x100000000; // 2^32
+        n += h * 0x100000000;
+        i++;
       }
-      return (n >>> 0) * 2.3283064365386963e-10; // 2^-32
+      return (n >>> 0) * 2.3283064365386963e-10;
     };
- 
-    mash.version = 'Mash 0.9';
+    
+    mash.version = "Mash 0.9";
     return mash;
-  }
-
-  function Alea() {
+  };
+  
+  Alea = function() {
     return (function(args) {
-      // Johannes BaagÃ¸e <baagoe@baagoe.com>, 2010
-      var s0 = 0;
-      var s1 = 0;
-      var s2 = 0;
-      var c = 1;
-
-      if (args.length == 0) {
-        args = [+new Date];
+      var c, i, mash, random, s0, s1, s2;
+      s0 = 0;
+      s1 = 0;
+      s2 = 0;
+      c = 1;
+      
+      if (args.length === 0) {
+        args = [+(new Date)];
       }
-      var mash = Mash();
-      s0 = mash(' ');
-      s1 = mash(' ');
-      s2 = mash(' ');
-
-      for (var i = 0; i < args.length; i++) {
+      
+      mash = Mash();
+      s0 = mash(" ");
+      s1 = mash(" ");
+      s2 = mash(" ");
+      i = 0;
+      
+      while (i < args.length) {
         s0 -= mash(args[i]);
         if (s0 < 0) {
           s0 += 1;
@@ -55,69 +60,91 @@ var HangoutDice = (function() {
         if (s2 < 0) {
           s2 += 1;
         }
+        i++;
       }
+      
       mash = null;
-
-      var random = function() {
-        var t = 2091639 * s0 + c * 2.3283064365386963e-10; // 2^-32
+      
+      random = function() {
+        var t;
+        t = 2091639 * s0 + c * 2.3283064365386963e-10;
         s0 = s1;
         s1 = s2;
         return s2 = t - (c = t | 0);
       };
+      
       random.uint32 = function() {
-        return random() * 0x100000000; // 2^32
+        return random() * 0x100000000;
       };
+      
       random.fract53 = function() {
-        return random() + 
-          (random() * 0x200000 | 0) * 1.1102230246251565e-16; // 2^-53
+        return random() + (random() * 0x200000 | 0) * 1.1102230246251565e-16;
       };
-      random.version = 'Alea 0.9';
+      
+      random.version = "Alea 0.9";
       random.args = args;
       return random;
-
-    } (Array.prototype.slice.call(arguments)));
+    })(Array.prototype.slice.call(arguments_));
   };
-
-  var rollKey = "HangoutDice.roll";
-
+  
+  id = null;
+  dice = {};
+  callbacks = {};
+  rollKey = "HangoutDice.roll";
+  
   dice.roll = function(num, sides, callback) {
-    var timestamp = (+new Date);
-    if (callback)
+    var delta, rollPayload, timestamp;
+    timestamp = +(new Date);
+    
+    if (callback) {
       callbacks[timestamp] = callback;
-    var rollPayload = JSON.stringify({
+    }
+    
+    rollPayload = JSON.stringify({
       timestamp: timestamp,
       id: id,
       num: num,
       sides: sides
     });
-    var delta = {};
+    
+    delta = {};
     delta[rollKey] = rollPayload;
-    gapi.hangout.data.submitDelta(delta, []);
-  }
-
-  dice.onRoll = function() {}
-
+    return gapi.hangout.data.submitDelta(delta, []);
+  };
+  
+  dice.onRoll = function() {};
+  
   gapi.hangout.onApiReady.add(function() {
     id = gapi.hangout.getParticipantId();
-
-    gapi.hangout.data.onStateChanged.add(function(e) {
-      for(var idx = 0; idx < e.addedKeys.length; idx++) {
-        var obj = e.addedKeys[idx];
-        if (obj.key == rollKey) {
-          var roll = JSON.parse(obj.value);
+    
+    return gapi.hangout.data.onStateChanged.add(function(e) {
+      var i, idx, obj, rand, roll, _results;
+      idx = 0;
+      _results = [];
+      
+      while (idx < e.addedKeys.length) {
+        obj = e.addedKeys[idx];
+        if (obj.key === rollKey) {
+          roll = JSON.parse(obj.value);
           roll.rolls = [];
-          var rand = Alea(obj.timestamp);
-          for (var i=0;i<roll.num;i++)
+          rand = Alea(obj.timestamp);
+          i = 0;
+          
+          while (i < roll.num) {
             roll.rolls.push((rand.uint32() % roll.sides) + 1);
+            i++;
+          }
+          
           if (callbacks[roll.timestamp]) {
             callbacks[roll.timestamp](roll, rand);
             callbacks[roll.timestamp] = null;
           }
           dice.onRoll(roll, rand);
         }
+        _results.push(idx++);
       }
+      return _results;
     });
   });
-
   return dice;
 })();
